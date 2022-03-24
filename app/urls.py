601762bf3004ded0bbfedd5e3425126ff456ff2e
Jan Baykara <janbaykara@pm.me>
2022-03-24 18:21:47 +0000

    Remove dummy content

--- app/urls.py
+++ app/urls.py
@@ -9,21 +9,6 @@ from wagtail.core import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
-map = {
-    "layer": {
-        "id": "terrain-data",
-        "type": "line",
-        "source": "mapbox-terrain",
-        "source-layer": "contour",
-        "layout": {"line-join": "round", "line-cap": "round"},
-        "paint": {"line-color": "#ff69b4", "line-width": 1},
-    },
-    "data": {
-        "type": "vector",
-        "url": "mapbox://mapbox.mapbox-terrain-v2",
-    },
-}
-
 
 urlpatterns = [
     path("django/", admin.site.urls),

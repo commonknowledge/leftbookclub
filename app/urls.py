ce40802d3d0e1d03d8a4e54434a0bff3eb7f99c7
Jan Baykara <janbaykara@pm.me>
2022-03-24 21:58:52 +0000

    Add Favicon

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path, re_path
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 from wagtail.core import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
@@ -13,7 +14,7 @@ urlpatterns = [
     path("django/", admin.site.urls),
     path('admin/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
-    path("__debug__/", include(debug_toolbar.urls)),
+    path('favicon.ico', RedirectView.as_view(url='/static/images/logo.png', permanent=True)),
     # For anything not caught by a more specific rule above, hand over to Wagtail's serving mechanism
     re_path(r'', include(wagtail_urls)),
 ]

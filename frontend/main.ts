cdae5572b0d7eb852d32ad619afb8293bc11e4e7
Jan Baykara <janbaykara@pm.me>
2022-03-28 09:14:34 +0100

    Upgrade groundwork-ui JS dependency

--- frontend/main.ts
+++ frontend/main.ts
@@ -2,7 +2,7 @@ import 'vite/modulepreload-polyfill'
 import "./scss/main.scss";
 import "@hotwired/turbo";
 
-import startApp from "groundwork-ui";
+import { startApp } from "groundwork-ui";
 
 const controllers = import.meta.glob("./controllers/*-controller.ts");
 

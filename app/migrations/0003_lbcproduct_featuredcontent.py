0369680c6010d8a6b0c19963db7f1eba1b04fdc6
Jan Baykara <janbaykara@pm.me>
2022-03-31 17:15:58 +0100

    Fix migration

--- app/migrations/0003_lbcproduct_featuredcontent.py
+++ app/migrations/0003_lbcproduct_featuredcontent.py
@@ -8,7 +8,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ("wagtailcore", "0066_collection_management_permissions"),
-        ("djstripe", "0011_alter_invoice_charge_alter_invoice_customer_and_more"),
+        ("djstripe", "0010_alter_customer_balance"),
         ("app", "0002_homepage"),
     ]
 

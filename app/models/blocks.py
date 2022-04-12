f8f3e768a0567234ca3ab723a45af06b3306d0d7
Moggach <acunnane13@gmail.com>
2022-04-12 16:22:22 +0000

    adding template for embed block and removing separate image definition from article content stream

--- app/models/blocks.py
+++ app/models/blocks.py
@@ -2,16 +2,6 @@ from wagtail.core import blocks
 from wagtail.core.blocks import RichTextBlock
 from wagtail.core.fields import StreamField
 from wagtail.embeds.blocks import EmbedBlock
-from wagtail.images.blocks import ImageChooserBlock
-
-
-class ImageBlock(blocks.StructBlock):
-    image = ImageChooserBlock(required=False)
-    caption = blocks.CharBlock()
-
-    class Meta:
-        template = "app/content/image.html"
-        icon = "image"
 
 
 def ArticleContentStream(block_types=None, **kwargs):
@@ -31,8 +21,7 @@ def ArticleContentStream(block_types=None, **kwargs):
                 template="app/content/text.html",
             ),
         ),
-        ("embed", EmbedBlock(max_width=800, max_height=400)),
-        ("image", ImageBlock()),
+        ("embed", EmbedBlock(template="app/content/embed.html")),
     ]
 
     return StreamField(common_block_types + (block_types or []), **kwargs)

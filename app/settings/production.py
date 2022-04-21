8a8df9ab3b4193b22c2ee72c039b0ccf1d63b093
Jan Baykara <janbaykara@pm.me>
2022-04-21 14:31:31 +0100

    Add mailgun config option

--- app/settings/production.py
+++ app/settings/production.py
@@ -10,16 +10,21 @@ CSRF_TRUSTED_ORIGINS = [os.getenv("BASE_URL", "")]
 if os.getenv("AWS_S3_REGION_NAME"):
     DEFAULT_FILE_STORAGE = "app.storage.DigitalOceanSpacesStorage"
     AWS_S3_ADDRESSING_STYLE = "virtual"
+    # like `fra1`
     AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
+    # like `leftbookclub`
     AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+    # like `https://fra1.digitaloceanspaces.com`
     AWS_S3_ENDPOINT_URL = os.getenv("AWS_S3_ENDPOINT_URL")
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    # can be ignored
     AWS_S3_CUSTOM_DOMAIN = os.getenv("AWS_S3_CUSTOM_DOMAIN")
     MEDIA_URL = os.getenv("MEDIA_URL")
 else:
     MEDIA_ROOT = os.getenv(MEDIA_ROOT)
     MEDIA_URL = os.getenv("MEDIA_URL", "/media")
+
 
 if os.getenv("MAILJET_API_KEY"):
     # if you don't already have this in settings
@@ -27,11 +32,22 @@ if os.getenv("MAILJET_API_KEY"):
     # ditto (default from-email for Django errors)
     SERVER_EMAIL = DEFAULT_FROM_EMAIL
     ANYMAIL = {
+        "SEND_DEFAULTS": {"envelope_sender": DEFAULT_FROM_EMAIL},
         "MAILJET_API_KEY": os.getenv("MAILJET_API_KEY"),
         "MAILJET_SECRET_KEY": os.getenv("MAILJET_SECRET_KEY"),
-        "SEND_DEFAULTS": {"envelope_sender": DEFAULT_FROM_EMAIL},
     }
     EMAIL_BACKEND = "anymail.backends.mailjet.EmailBackend"
+elif os.getenv("MAILGUN_API_URL"):
+    ANYMAIL = {
+        "MAILGUN_API_URL": os.getenv("MAILGUN_API_URL"),
+        "MAILGUN_API_KEY": os.getenv("MAILGUN_API_KEY"),
+        "MAILGUN_SENDER_DOMAIN": os.getenv("MAILGUN_SENDER_DOMAIN"),
+    }
+    EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
+    DEFAULT_FROM_EMAIL = f"noreply@{ANYMAIL['MAILGUN_SENDER_DOMAIN']}"
+    ANYMAIL["SEND_DEFAULTS"] = ({"envelope_sender": DEFAULT_FROM_EMAIL},)
+    # ditto (default from-email for Django errors)
+    SERVER_EMAIL = f"admin@{ANYMAIL['MAILGUN_SENDER_DOMAIN']}"
 
 WAGTAILTRANSFER_SECRET_KEY = os.getenv("WAGTAILTRANSFER_SECRET_KEY")
 

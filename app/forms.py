eec0ec719a8b03db73ee0e5762e80ffe2378d315
Jan Baykara <janbaykara@pm.me>
2022-04-25 16:04:35 +0100

    Add shipping form

--- app/forms.py
+++ app/forms.py
@@ -31,3 +31,52 @@ class GiftCodeForm(forms.Form):
                 raise ValidationError("This gift code has already been redeemed")
             else:
                 raise ValidationError("This gift code isn't valid")
+
+
+class StripeShippingForm(forms.Form):
+    name = forms.CharField(label="Recipient name", max_length=250)
+    line1 = forms.CharField(
+        label="Address line 1 (e.g., street, PO Box, or company name)",
+        max_length=250,
+        required=False,
+        empty_value=None,
+    )
+    line2 = forms.CharField(
+        label="Address line 2 (e.g., apartment, suite, unit, or building)",
+        max_length=250,
+        required=False,
+        empty_value=None,
+    )
+    postal_code = forms.CharField(
+        label="ZIP or postal code", max_length=250, required=False, empty_value=None
+    )
+    city = forms.CharField(
+        label="City, district, suburb, town, or village",
+        max_length=250,
+        required=False,
+        empty_value=None,
+    )
+    state = forms.CharField(
+        label="State, county, province, or region",
+        max_length=250,
+        required=False,
+        empty_value=None,
+    )
+    country = CountryField().formfield(
+        label="Country", widget=CountrySelectWidget, required=False, empty_value=None
+    )
+
+    def clean(self):
+        self.cleaned_data = super().clean()
+        self.cleaned_data["shipping"] = {
+            "name": self.cleaned_data["name"],
+            "address": {
+                "line1": self.cleaned_data("line1", None),
+                "line2": self.cleaned_data("line2", None),
+                "postal_code": self.cleaned_data("postal_code", None),
+                "city": self.cleaned_data("city", None),
+                "state": self.cleaned_data("state", None),
+                "country": self.cleaned_data("country", None),
+            },
+        }
+        return self.cleaned_data

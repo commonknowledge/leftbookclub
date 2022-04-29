7c0b126c51727b3ce74b9bed6421a08bb6e37695
Jan Baykara <janbaykara@pm.me>
2022-04-29 01:43:23 +0100

    Add many more content blocks

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      spacing: {
+        1: "5px",
+        2: "10px",
+        3: "15px",
+        4: "20px",
+        5: "40px",
+        6: "80px",
+      },
       colors: {
         background: "#F2F0F0",
         black: "#231F20",

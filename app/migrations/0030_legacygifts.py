5fbac8cf8fca700e1d1894aa7c80e121fb602919
Jan Baykara <janbaykara@pm.me>
2022-04-30 19:26:03 +0100

    Fix migrations

--- app/migrations/0030_legacygifts.py
+++ app/migrations/0030_legacygifts.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("djstripe", "0011_alter_invoice_charge_alter_invoice_customer_and_more"),
+        ("djstripe", "0010_alter_customer_balance"),
         ("app", "0029_alter_user_old_id"),
     ]
 

36c4e04e54cfc0dae9d1c5c111b4da61ef3d0ef4
Jan Baykara <janbaykara@pm.me>
2022-05-01 22:34:07 +0100

    Test giving a gift to someone else

--- app/tests.py
+++ app/tests.py
@@ -274,7 +274,22 @@ class PlansAndShippingTestCase(TestCase):
 
 
 class GiftTestCase(TestCase):
-    def setUp(self) -> None:
+    users = []
+
+    @classmethod
+    def create_user(cls):
+        uid = "".join(random.choice(string.ascii_lowercase) for i in range(10))
+        user = User.objects.create_user(
+            uid, f"unit-test-{uid}@leftbookclub.com", "default_pw_12345_xyz_lbc"
+        )
+        password = User.objects.make_random_password()
+        user.set_password(password)
+        user.save(update_fields=["password"])
+        cls.users += [user]
+        return user
+
+    @classmethod
+    def setUpTestData(cls):
         # sync all products
         products = stripe.Product.list(limit=100)
         for product in products:
@@ -286,19 +301,14 @@ class GiftTestCase(TestCase):
             djstripe.models.Coupon.sync_from_stripe_data(coupon)
 
         # create user
-        uid = "".join(random.choice(string.ascii_lowercase) for i in range(10))
-        self.user = User.objects.create_user(
-            "UnitTest", f"unit-test-{uid}@leftbookclub.com", "default_pw_12345_xyz_lbc"
-        )
-        password = User.objects.make_random_password()
-        self.user.set_password(password)
-        self.user.save(update_fields=["password"])
+        cls.gift_giver_user = cls.create_user()
+        cls.gift_recipient_user = cls.create_user()
 
         # set up stripe customer details
         customer = stripe.Customer.create(
-            email=self.user.email, metadata={"text_mode": "true"}
+            email=cls.gift_giver_user.email, metadata={"text_mode": "true"}
         )
-        self.payment_card = stripe.PaymentMethod.create(
+        cls.payment_card = stripe.PaymentMethod.create(
             type="card",
             card={
                 "number": "4242424242424242",
@@ -308,16 +318,15 @@ class GiftTestCase(TestCase):
             },
         )
         stripe.PaymentMethod.attach(
-            self.payment_card.id,
+            cls.payment_card.id,
             customer=customer.id,
         )
         customer = djstripe.models.Customer.sync_from_stripe_data(customer)
-        customer.subscriber = self.user
+        customer.subscriber = cls.gift_giver_user
         customer.save()
-        self.assertIsNotNone(self.user.stripe_customer)
 
         # configure a gift plan
-        self.gift_plan = MembershipPlanPage(
+        cls.gift_plan = MembershipPlanPage(
             title="The Gift of Solidarity",
             deliveries_per_year=12,
             products=[],
@@ -329,22 +338,23 @@ class GiftTestCase(TestCase):
                 )
             ],
         )
-        Page.get_first_root_node().add_child(instance=self.gift_plan)
-        self.gift_plan.save()
+        Page.get_first_root_node().add_child(instance=cls.gift_plan)
+        cls.gift_plan.save()
         product = djstripe.models.Product.objects.filter(
             active=True, metadata__shipping__isnull=True
         ).first()
-        self.gift_plan.products.add(product)
-        self.assertIsNotNone(self.gift_plan.products.first())
+        cls.gift_plan.products.add(product)
 
-        return super().setUp()
+        return super().setUpTestData()
 
-    def tearDown(self) -> None:
-        if self.user.stripe_customer is not None:
-            stripe.Customer.delete(self.user.stripe_customer.id)
-        return super().tearDown()
+    @classmethod
+    def tearDownClass(cls) -> None:
+        for user in cls.users:
+            if user.stripe_customer is not None:
+                stripe.Customer.delete(user.stripe_customer.id)
+        return super().tearDownClass()
 
-    def test_buying_gift_card(self):
+    def test_buying_gift_card_and_applying_it_to_yourself(self):
         # create subscription
         checkout_args = SubscriptionCheckoutView.create_checkout_args(
             product=self.gift_plan.products.first(),
@@ -353,7 +363,7 @@ class GiftTestCase(TestCase):
             gift_mode=True,
         )
         gift_giver_subscription = stripe.Subscription.create(
-            customer=self.user.stripe_customer.id,
+            customer=self.gift_giver_user.stripe_customer.id,
             items=checkout_args["line_items"],
             metadata={"automated_test_record": "true"},
             default_payment_method=self.payment_card.id,
@@ -366,11 +376,11 @@ class GiftTestCase(TestCase):
             gift_giver_subscription,
         ) = configure_gift_giver_subscription_and_code(
             gift_giver_subscription.id,
-            self.user.id,
+            self.gift_giver_user.id,
             metadata={"automated_test_record": "true"},
         )
         self.assertEqual(
-            self.user.active_subscription,
+            self.gift_giver_user.active_subscription,
             None,
             "Gift subscriptions should not count as active membership subscriptions for the gift purchaser.",
         )
@@ -394,7 +404,7 @@ class GiftTestCase(TestCase):
 
         # Test redemption on self
         recipient_subscription = create_gift_recipient_subscription(
-            gift_giver_subscription, self.user
+            gift_giver_subscription, self.gift_giver_user
         )
 
         # Assert that the recipient subscription is correctly set up in Stripe
@@ -419,7 +429,92 @@ class GiftTestCase(TestCase):
         self.assertEqual(recipient_subscription.metadata["promo_code"], promo_code.id)
 
         # Assert that this has trickled through to the user model methods
-        self.assertEqual(self.user.active_subscription, recipient_subscription)
+        self.assertEqual(
+            self.gift_giver_user.active_subscription, recipient_subscription
+        )
+
+        # Assert that the promo code can't be used anymore, because it's been redeemed
+        promo_code = stripe.PromotionCode.retrieve(promo_code.id)
+        self.assertFalse(promo_code.active)
+
+    def test_buying_gift_card_and_applying_it_to_yourself(self):
+        # create subscription
+        checkout_args = SubscriptionCheckoutView.create_checkout_args(
+            product=self.gift_plan.products.first(),
+            price=self.gift_plan.monthly_price,
+            zone=ShippingZone.default_zone,
+            gift_mode=True,
+        )
+        gift_giver_subscription = stripe.Subscription.create(
+            customer=self.gift_giver_user.stripe_customer.id,
+            items=checkout_args["line_items"],
+            metadata={"automated_test_record": "true"},
+            default_payment_method=self.payment_card.id,
+        )
+
+        # TODO: test line items are correct
+
+        (
+            promo_code,
+            gift_giver_subscription,
+        ) = configure_gift_giver_subscription_and_code(
+            gift_giver_subscription.id,
+            self.gift_giver_user.id,
+            metadata={"automated_test_record": "true"},
+        )
+        self.assertEqual(
+            self.gift_giver_user.active_subscription,
+            None,
+            "Gift subscriptions should not count as active membership subscriptions for the gift purchaser.",
+        )
+
+        # Test that promo code and updated_subscription have correct metadata
+        self.assertEqual(
+            gift_giver_subscription.metadata["automated_test_record"],
+            "true",
+            "configure_gift_giver_subscription_and_code() should pass through custom metadata -- to subscription",
+        )
+        self.assertIsNotNone(gift_giver_subscription.metadata["gift_mode"])
+        self.assertEqual(gift_giver_subscription.metadata["promo_code"], promo_code.id)
+        self.assertEqual(
+            promo_code.metadata["gift_giver_subscription"], gift_giver_subscription.id
+        )
+        self.assertEqual(
+            promo_code.metadata["automated_test_record"],
+            "true",
+            "configure_gift_giver_subscription_and_code() should pass through custom metadata -- to promo_code",
+        )
+
+        # Test redemption on self
+        recipient_subscription = create_gift_recipient_subscription(
+            gift_giver_subscription, self.gift_recipient_user
+        )
+
+        # Assert that the recipient subscription is correctly set up in Stripe
+        self.assertEqual(
+            recipient_subscription.status, djstripe.enums.SubscriptionStatus.active
+        )
+        self.assertNotEqual(recipient_subscription.discount, None)
+        self.assertEqual(
+            get_primary_product_for_djstripe_subscription(gift_giver_subscription),
+            get_primary_product_for_djstripe_subscription(recipient_subscription),
+        )
+        self.assertEqual(
+            get_shipping_product_for_djstripe_subscription(gift_giver_subscription),
+            get_shipping_product_for_djstripe_subscription(recipient_subscription),
+        )
+
+        # Assert metadata on gift recipient sub is correct
+        self.assertEqual(
+            recipient_subscription.metadata["gift_giver_subscription"],
+            gift_giver_subscription.id,
+        )
+        self.assertEqual(recipient_subscription.metadata["promo_code"], promo_code.id)
+
+        # Assert that this has trickled through to the user model methods
+        self.assertEqual(
+            self.gift_recipient_user.active_subscription, recipient_subscription
+        )
 
         # Assert that the promo code can't be used anymore, because it's been redeemed
         promo_code = stripe.PromotionCode.retrieve(promo_code.id)

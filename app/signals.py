961099e74fa5c1a528304ac7ee668ee6d65f7a3e
Jan Baykara <janbaykara@pm.me>
2022-05-02 00:43:01 +0100

    Fix webhook

--- app/signals.py
+++ app/signals.py
@@ -7,11 +7,12 @@ from app.utils.stripe import gift_recipient_subscription_from_code
 @webhooks.handler("customer.subscription.deleted")
 def cancel_gift_recipient_subscription(event, **kwargs):
     # update their situation on mailchimp
+    object = event.data.get("object", {})
     if (
-        event.subscription is not None
-        and event.subscription.metadata.get("gift_mode", None) is not None
+        object.get("object", None) == "subscription"
+        and object.get("metadata", {}).get("gift_mode", None) is not None
     ):
-        promo_code = event.subscription.metadata.get("promo_code", None)
+        promo_code = object.get("metadata", {}).get("promo_code", None)
         recipient_subscription = gift_recipient_subscription_from_code(promo_code)
         stripe.Subcription.delete(recipient_subscription.id)
         # TODO: send them an email

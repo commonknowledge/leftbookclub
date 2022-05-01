ddb9d7a2a3180622cdb1aeb38d92f193415b08e4
Jan Baykara <janbaykara@pm.me>
2022-05-01 23:03:07 +0100

    Don't refresh stripe data for static file requests (which happen predominantly in development)

--- app/middleware.py
+++ app/middleware.py
@@ -1,11 +1,15 @@
+from django.conf import settings
+
+
 def update_stripe_customer_subscription(get_response):
     # One-time configuration and initialization.
 
     def middleware(request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
-        if request.user.is_authenticated:
-            request.user.refresh_stripe_data()
+        if not request.path.startswith(settings.STATIC_URL):
+            # Code to be executed for each request before
+            # the view (and later middleware) are called.
+            if request.user.is_authenticated:
+                request.user.refresh_stripe_data()
 
         response = get_response(request)
 

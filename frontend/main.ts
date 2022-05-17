8d9efbe3e970a156835b99b12d6e1f4b54569a70
Jan Baykara <janbaykara@pm.me>
2022-05-17 18:19:50 +0100

    Disable Turbo sessions to see if that fixes account issues

--- frontend/main.ts
+++ frontend/main.ts
@@ -1,10 +1,12 @@
 import "./scss/main.scss";
-import "@hotwired/turbo";
+import Turbo from "@hotwired/turbo";
 import "bootstrap";
 import initialiseSentry from "./sentry";
 import initialisePosthog from "./posthog";
 import { startApp } from "groundwork-ui";
 const controllers = import.meta.glob("./controllers/*-controller.ts");
+Turbo.session.drive = false;
+console.log(Turbo);
 
 initialisePosthog();
 initialiseSentry();

0e2f6c8fcb57f8ad5ecb6daa340823bce2e0f344
Joaquim d'Souza <joaquimds@gmail.com>
2025-03-31 19:34:57 +0200

    fix: separate worker and cron worker, as the worker blocks

---
+++ app/management/commands/start_cron_worker.py
@@ -0,0 +1,22 @@
+from datetime import timedelta
+from django.core import management
+from django.core.management.base import BaseCommand
+from groundwork.core.cron import register_cron
+
+
+class Command(BaseCommand):
+    help = "Run background processes"
+
+    def handle(self, *args, **options):
+        # Register cron commands
+        def run_ensure_stripe_subscriptions_processed():
+            management.call_command(
+                "ensure_stripe_subscriptions_processed", dry_run=True
+            )
+
+        register_cron(run_ensure_stripe_subscriptions_processed, timedelta(seconds=30))
+
+        print("Starting cron worker")
+
+        # Start the periodic job queue (`groundwork` via `schedule`)
+        management.call_command("run_cron_tasks")
--- app/management/commands/start_worker.py
+++ app/management/commands/start_worker.py
@@ -1,7 +1,5 @@
-from datetime import timedelta
 from django.core import management
 from django.core.management.base import BaseCommand
-from groundwork.core.cron import register_cron
 
 
 class Command(BaseCommand):
@@ -10,15 +8,3 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # Start the one-off job queue (`django_dbq`)
         management.call_command("worker", rate_limit=30)
-
-        # Register cron commands
-        def run_ensure_stripe_subscriptions_processed():
-            management.call_command(
-                "ensure_stripe_subscriptions_processed", dry_run=True
-            )
-
-        register_cron(run_ensure_stripe_subscriptions_processed, timedelta(seconds=60))
-
-        # Start the periodic job queue (`groundwork` via `schedule`)
-        management.call_command("run_cron_tasks")
-        print("Workers have been started.")

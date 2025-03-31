0571af16185a78872431ea4a26ed84fa9df6da7d
Moggach <acunnane13@gmail.com>
2025-03-31 16:23:42 +0000

    update command to loop through customers in stripe test mode

--- app/management/commands/ensure_stripe_subscriptions_processed.py
+++ app/management/commands/ensure_stripe_subscriptions_processed.py
@@ -6,51 +6,54 @@ import stripe
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        users = User.objects.order_by("email")
-        for user in users:
-            print(f"Checking user {user.email}")
-            try:
-                if not user.stripe_customer:
-                    print(f"User {user.email}: not a stripe customer, skipping\n")
-                    continue
-                sub = user.stripe_customer.subscriptions.first()
-                if not sub:
-                    print(f"User {user.email}: no subscription, skipping\n")
-                    continue
-                if sub.metadata.get("processed"):
-                    print(f"User {user.email}: subscription already processed, skipping\n")
-                    continue
+        customers = stripe.Customer.list(limit=100)
+    
+        for customer in customers.auto_paging_iter():
+            subscriptions = stripe.Subscription.list(customer=customer.id)
+            for subscription in subscriptions.auto_paging_iter():
+                print(f"Customer: {customer.email}")
+                print(f"  Subscription ID: {subscription.id}")
+                print(f"  Status: {subscription.status}")
+                try:
+                    print(f"Customer {customer.email} metadata: {subscription.metadata}")
+                    if not subscription.metadata:
+                        user = User.objects.filter(email=customer.email).first()
+                        if user:
+                            if user.primary_product:
+                                
+                                create_shopify_order(
+                                    customer,
+                                    line_items=[
+                                        {
+                                            "title": f"Membership Subscription Purchase — {user.primary_product.name}",
+                                            "quantity": 1,
+                                            "price": 0,
+                                        }
+                                    ],
+                                    tags=["Membership Subscription Purchase", "Manual Sync"],
+                                )
+                                print(print(f"Customer {customer.email} created shopify order {user.primary_product.name}"))
+                               
+                                
+                            else:
+                                print(f"Customer {customer.email} has no primary product")
+                            
+                            stripe.Subscription.modify(subscription.id, metadata={"processed": "True"})
+                            print(f"Customer {customer.email} subscription processed")
+                        else:
+                            print(f"Customer {customer.email} user not found\n")
+                    else:
+                        print(f"Customer {customer.email}: already processed.\n")
+                    
+                except Exception as e:
+                    from sentry_sdk import capture_exception, capture_message
+                    
+                    print(f"User {customer.email} error: {e}")
 
-                print(f"User {user.email}: processing subscription...")
-                
-                if not user.primary_product:
-                    print(f"User {user.email}: has no primary product, skipping\n")
-                    continue
 
-                create_shopify_order(
-                    user,
-                    line_items=[
-                        {
-                            "title": f"Membership Subscription Purchase — {user.primary_product.name}",
-                            "quantity": 1,
-                            "price": 0,
-                        }
-                    ],
-                    tags=["Membership Subscription Purchase", "Manual Sync"],
-                )
-                stripe.Subscription.modify(sub.id, metadata={"processed": "True"})
-
-                print(f"User {user.email}: processed.\n")
-                
-            except Exception as e:
-                from sentry_sdk import capture_exception, capture_message
-                
-                print(f"User {user.email} error: {e}")
-
-
-                # Log to Sentry
-                capture_exception(e)
-                capture_message(
-                    f"[StripeCheckoutSuccess] Failed to complete processing for user {user.email}"
-                )
+                    # Log to Sentry
+                    capture_exception(e)
+                    capture_message(
+                        f"[StripeCheckoutSuccess] Failed to complete processing for user {customer.email}"
+                    )
 

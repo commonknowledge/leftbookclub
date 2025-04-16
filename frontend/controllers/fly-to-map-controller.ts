c67b6614c20b54a6318764350c6929cc934bf29c
Moggach <acunnane13@gmail.com>
2025-04-16 15:45:14 +0000

    fly to map on group button click rather than mouse enter

--- frontend/controllers/fly-to-map-controller.ts
+++ frontend/controllers/fly-to-map-controller.ts
@@ -1,18 +1,25 @@
-// import { MapConfigController } from "groundwork-django";
+import { MapConfigController } from "groundwork-django";
 
-// export default class MapHtmlBridgeController extends MapConfigController {
-//   static values = {
-//     zoom: { type: Number, default: 12 },
-//   };
+export default class MapHtmlBridgeController extends MapConfigController {
+  static values = {
+    zoom: { type: Number, default: 12 },
+  };
 
-//   private zoomValue!: number;
+  private zoomValue!: number;
 
-//   flyTo({ params: { lng, lat, zoom = this.zoomValue } }: any) {
-//     if (!!lng && !!lat) {
-//       this.map?.flyTo({
-//         center: [lng, lat],
-//         zoom: zoom,
-//       });
-//     }
-//   }
-// }
+  flyTo(event: any) {
+    const button = event.currentTarget;
+    const lng = parseFloat(button.dataset.lng);
+    const lat = parseFloat(button.dataset.lat);
+    const zoom = button.dataset.zoom
+      ? parseFloat(button.dataset.zoom)
+      : this.zoomValue;
+
+    if (!isNaN(lng) && !isNaN(lat)) {
+      this.map?.flyTo({
+        center: [lng, lat],
+        zoom: zoom,
+      });
+    }
+  }
+}
